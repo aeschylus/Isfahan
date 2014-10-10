22c697f6d976432b6c4faccb63af346b0dbd8098
aeschylus <scipioaffricanus@gmail.com>
2014-10-09 17:54:03 -0700

    Padding function repaired. A previous commit closed #1. This one closes #2.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,11 @@ var Isfahan = function(configObject) {
 
   containerSize = (function(containerId) {
     return [document.getElementById(containerId).offsetWidth,
-    document.getElementById(containerId).offsetHeight]
+      document.getElementById(containerId).offsetHeight]
   })(configObject.containerId),
   pad = d3_layout_cellPadNull,
   round = Math.round,
+  padding = configObject.padding,
   hierarchy = d3.layout.hierarchy(),
   layoutDescription = configObject.layoutDescription;
 
@@ -20,7 +21,7 @@ var Isfahan = function(configObject) {
       remaining = children.slice(), // copy-on-write
       child,
       n;
-      
+
       while ((n = remaining.length) > 0) {
         group.push(child = remaining[n - 1]);
         remaining.pop();
@@ -29,22 +30,22 @@ var Isfahan = function(configObject) {
       children.forEach(calculateLayout);
     }
   }
-  
+
   // Positions the specified row of nodes. Modifies `rect`.
   function position(group, type, rect) {
     console.log('%c\n'+' parent ' + type + ' rect is: ', 'background: #222; color: #EFEFEF; font-family: helvetica neue; font-size:20px; padding: 0 7px 3px 0;');
     console.log(rect);
     var i = -1,
-        n = group.length,
-        x = rect.x,
-        y = rect.y,
-        o;
+    n = group.length,
+    x = rect.x,
+    y = rect.y,
+    o;
     if (type === "column") { // vertical subdivision (children are rows)
       console.log('\n'+'calculating child rows');
       while (++i < n) {
         o = group[n-(i+1)];
         o.x = x;
-        o.y = y + (rect.dy/n) * i; // TODO: adjust to siblings.
+        o.y = y + (rect.dy/n) * i; 
         o.dx = rect.dx;
         o.dy = rect.dy/n;
         console.log({x:o.x, y: o.y, width:o.dx, height: o.dy});
@@ -53,7 +54,7 @@ var Isfahan = function(configObject) {
       console.log('\n'+'calculating child columns');
       while (++i < n) {
         o = group[n-(i+1)];
-        o.x = x + (rect.dx/n) * i; // TODO: adjust to siblings.
+        o.x = x + (rect.dx/n) * i; 
         o.y = y;
         o.dx = rect.dx/n;
         o.dy = rect.dy;
@@ -77,7 +78,10 @@ var Isfahan = function(configObject) {
     root.dy = containerSize[1];
 
     calculateLayout(root);
-    isfahan.nodes = nodes;
+    isfahan.padding(2);
+    nodes = nodes.map(function(node) {
+      return merge(node, pad(node));
+    });
 
     return nodes;
   } 
@@ -87,13 +91,13 @@ var Isfahan = function(configObject) {
     containerSize = x;
     return isfahan;
   };
-  
+
   isfahan.round = function(x) {
     if (!arguments.length) return round != Number;
     round = x ? Math.round : Number;
     return isfahan;
   };
-  
+
   isfahan.padding = function(x) {
     if (!arguments.length) return padding;
 
@@ -101,8 +105,8 @@ var Isfahan = function(configObject) {
       var p = x.call(isfahan, node, node.depth);
       console.log(p);
       return p == null
-          ? d3_layout_cellPadNull(node)
-          : d3_layout_cellPad(node, typeof p === "number" ? [p, p, p, p] : p);
+      ? d3_layout_cellPadNull(node)
+      : d3_layout_cellPad(node, typeof p === "number" ? [p, p, p, p] : p);
     }
 
     function padConstant(node) {
@@ -111,9 +115,9 @@ var Isfahan = function(configObject) {
 
     var type;
     pad = (padding = x) == null ? d3_layout_cellPadNull
-        : (type = typeof x) === "function" ? padFunction
-        : type === "number" ? (x = [x, x, x, x], padConstant)
-        : padConstant;
+    : (type = typeof x) === "function" ? padFunction
+    : type === "number" ? (x = [x, x, x, x], padConstant)
+    : padConstant;
     return isfahan;
   };
 
@@ -130,6 +134,39 @@ var Isfahan = function(configObject) {
     if (dy < 0) { y += dy / 2; dy = 0; }
     return {x: x, y: y, dx: dx, dy: dy};
   }
+  
+  function merge(target, source) {
+
+    /* Merges two (or more) objects,
+       giving the last one precedence */
+
+    if ( typeof target !== 'object' ) {
+      target = {};
+    }
+
+    for (var property in source) {
+
+      if ( source.hasOwnProperty(property) ) {
+
+        var sourceProperty = source[ property ];
+
+        if ( typeof sourceProperty === 'object' ) {
+          target[ property ] = util.merge( target[ property ], sourceProperty );
+          continue;
+        }
+
+        target[ property ] = sourceProperty;
+
+      }
+
+    }
+
+    for (var a = 2, l = arguments.length; a < l; a++) {
+      merge(target, arguments[a]);
+    }
+
+    return target;
+  };
 
   // return d3.rebind(isfahan, hierarchy,"sort", "children", "value");
   return isfahan(configObject);
